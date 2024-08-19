339438cc97547320f59ca8c85b83ba0a9fc501c3
ritikkr <ritikkumar2405199@gmail.com>
2024-08-20 04:23:36 +0530

    app nav

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
+  const navigate = useNavigate()
+  
+  function handleNavigateApp (){
+    navigate('app')
+  }
+
   return (
-    <div>Home</div>
+
+    <div>
+      Home
+    <button onClick={handleNavigateApp}>App </button>
+
+    </div>
   )
 }
 
